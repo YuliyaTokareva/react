bc1bb452bec37a6b865647d1a001168eb27a0bf0
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-08 00:24:17 +0300

    add task2

--- lesson3/task2/src/Search.jsx
+++ lesson3/task2/src/Search.jsx
@@ -2,7 +2,7 @@
  import "./search.scss";
 
 
-const Search = (props) =>{
+function Search(props) {
   return (
     <div className="search">
     <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
