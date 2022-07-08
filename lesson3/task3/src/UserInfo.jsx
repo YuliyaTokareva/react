dbad71e74ed93910d810b8f80118183f1ad00cad
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-08 14:24:07 +0300

    add

--- lesson3/task3/src/UserInfo.jsx
+++ lesson3/task3/src/UserInfo.jsx
@@ -4,8 +4,11 @@ import Avatar  from "./Avatar.jsx";
 const UserInfo = props =>{
 
 return (<div className="user-info">
-        <Avatar user ={props.user} />
+        <Avatar name={props.user.name} 
+        avatarUrl={props.user.avatarUrl}
+        />
         <div className="user-info__name">{props.user.name}</div>
+        
       </div>)
 }
 
