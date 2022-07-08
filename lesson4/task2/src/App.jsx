6da698320841830e63349a66dfd93b09e03c2e72
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-09 02:05:09 +0300

    add

---
+++ lesson4/task2/src/App.jsx
@@ -0,0 +1,22 @@
+ import React from "react";
+import Clock from './Clock.jsx'
+
+
+const App = () =>{
+  
+  return (
+    <>
+    <Clock  offset={0} location="London"/>
+    <Clock  offset={2} location="Kyiv"/>
+    <Clock  offset={-5} location="New York"/>
+    </>
+         
+ 
+  
+   
+  );
+}
+
+
+
+ export default App;
---
+++ lesson4/task2/src/Clock.jsx
@@ -0,0 +1,52 @@
+import React, { Component} from "react";
+ import './clock.scss'
+ import moment from "moment";
+
+import 'moment-timezone';
+
+
+
+ const date = new Date();
+ 
+const timeZone = new Date(date.setUTCHours(-5));
+const formatDate = date => moment(date).format("HH:mm:ss");
+const zone = moment(date);
+
+//console.log(formatDate(a));
+
+class Clock extends Component {
+  constructor(props){
+    super(props);
+    this.hour = props.offset
+    this.timeZone = date.setUTCHours(this.hour);
+    this.format = moment(this.timeZone).format("HH:mm:ss")
+
+    this.state = {
+      counter:  this.timeZone,
+    };
+  
+console.log(props.location);
+  setInterval(() => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+}, 1000);
+}
+
+  render() {
+    return (
+          <div className="clock">
+    <div className="clock__location">
+        {this.props.location}
+    </div>
+    <div className="clock__time">
+        {this.state.counter}
+    </div>
+</div>
+  
+  );
+  }
+
+}
+
+ export default Clock;
