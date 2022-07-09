05a1035037305f628b57bdd79d5990ee6b8a36a1
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-10 01:04:28 +0300

    add

---
+++ lesson5/task5/src/Toggler.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+
+class Toggler extends Component {
+  constructor(props){
+  super(props)
+
+  this.state = {
+    counter: "On",
+  }
+  this.decrement = this.decrement.bind(this)
+  
+}
+
+decrement(e){
+      const textNow = e.target.textContent;
+       textNow === "On" ? 
+      this.setState({
+      counter: "Off",
+      })
+      : 
+      this.setState({
+      counter: "On",
+      })
+}
+
+  render() {
+    return (
+     <div className="toggler"
+     onClick={this.decrement}
+     >{this.state.counter}</div>   
+  );
+  }
+
+}
+
+ export default Toggler;
