ff21d4b003613c3cd5b548fee67c130ebd1d3462
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-09 18:05:06 +0300

    add

---
+++ lesson5/task1/src/GoodButton.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+
+class GoodButton extends Component {
+ 
+
+
+
+  render() {
+    return (
+          <button className="fancy-button"
+          onClick = {()=>
+            alert('Good job!')
+          }
+          >Click me!</button>
+  
+  );
+  }
+
+}
+
+ export default GoodButton;
