81a4258877458b38a91d8e6d81a19a07ffad1814
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-15 01:54:30 +0300

    add

---
+++ lesson8/task3/src/Dimensions.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Dimensions extends Component {
+    state = {
+        width: null,
+        height: null,
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.onResize);
+        const { innerWidth, innerHeight } = window;
+        this.setDimentions(innerWidth, innerHeight);
+    }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.onResize);
+    }
+    onResize = (e) => {
+        const { innerWidth, innerHeight } = e.target;
+        this.setDimentions(innerWidth, innerHeight);
+        // document.title = `${innerWidth} - ${innerHeight}`;
+    };
+    setDimentions = (width, height) => {
+        this.setState({
+            width,
+            height,
+        });
+        document.title = `${width} - ${height}`;
+    };
+
+    render() {
+        return (
+            <div className='dimensions'>
+                {`${this.state.width} - ${this.state.height}`}
+            </div>
+        );
+    }
+}
+
+export default Dimensions;
+
+///Первое прохождение
+//////нажали на кнопку Show
+// ----constructor,
+// ----render(),
+// ----componentDidMount;
+
+//Второе прохождение
+/////После нажатия кнопки 'Update'
+// ----shouldComponentUpdate();
+// ----render();
+// ----componentDidUpdate()
+
+// После нажатия кнопки 'Hide'
+// ----componentWillUnmount()
