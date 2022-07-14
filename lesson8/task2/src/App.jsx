0f1746e1ffdfc230f49e459f83f1ecbd03523c46
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-14 18:36:38 +0300

    add

--- lesson8/task2/src/App.jsx
+++ lesson8/task2/src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
     return (
         <>
             <Clock offset={0} location='London' />
+            <Clock offset={2} location='Kyiv' />
+            <Clock offset={-5} location='New York' />
         </>
     );
 };
