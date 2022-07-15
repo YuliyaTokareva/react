0380bec588aab1cb5cbc1f97d4d91f009d31acef
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-15 15:56:58 +0300

    add

---
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class ConnectionStatus extends Component {
+    state = {
+        connection: true,
+    };
+    componentDidMount() {
+        window.addEventListener("offline", this.setConnection);
+        window.addEventListener("online", this.setConnection);
+    }
+    componentWillUnmount() {
+        window.removeEventListener("offline", this.setConnection);
+        window.removeEventListener("online", this.setConnection);
+    }
+
+    setConnection = () => {
+        this.setState({
+            connection: !this.state.connection,
+        });
+    };
+
+    render() {
+        return (
+            <div className='status status_offline'>
+                {this.state.connection ? "Online" : "Offline"}
+            </div>
+        );
+    }
+}
+
+export default ConnectionStatus;
+
+///Первое прохождение
+//////нажали на кнопку Show
+// ----constructor,
+// ----render(),
+// ----componentDidMount;
+
+//Второе прохождение
+/////После нажатия кнопки 'Update'
+// ----shouldComponentUpdate();
+// ----render();
+// ----componentDidUpdate()
+
+// После нажатия кнопки 'Hide'
+// ----componentWillUnmount()
