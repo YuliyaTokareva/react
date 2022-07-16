85e4d1bd92776130d8b071a67c7ab820bc56343d
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-17 00:48:07 +0300

    add

---
+++ lesson9/task3/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Search from "./UserForm.jsx";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    createUser = (obj) => {
+        console.log(this.obj);
+    };
+    render() {
+        return <Search onSubmit={this.createUser} />;
+    }
+}
+
+export default App;
+
+// UserForm - отрисовка формы, вызывает ф-ю в onSubmit (controlled) состояние должно быть тут.
+// App = отрисовует UserForm. должен быть метод  createUser который принимает объект данных и выводит в консоль
