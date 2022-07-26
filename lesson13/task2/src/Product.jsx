15e0287fa105c3cc2ef12a0ca1dc825f831e43fd
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-26 10:28:58 +0300

    add

--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Product({ match }) {
-    return <div className="product">{`Product is ${match.params.productId}`}</div>
+    return <div className="product">{match.params.productId}</div>
 }
 
 export default Product
