78a85c976b686b26fdf483df4acb4051828bb4fc
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-27 18:20:04 +0300

    add

---
+++ lesson14/task3/src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import Dimensions from './Dimensions.jsx'
+
+const App = () => {
+    return <Dimensions />
+}
+
+export default App
---
+++ lesson14/task3/src/Dimensions.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const Dimensions = () => {
+    const [dimentions, setDimentions] = useState({
+        width: null,
+        height: null,
+    })
+    const { width, height } = dimentions
+    useEffect(() => {
+        const { innerWidth, innerHeight } = window
+        setDimentions({ width: innerWidth, height: innerHeight })
+        const onResize = (e) => {
+            const { innerWidth, innerHeight } = e.target
+            setDimentions({ width: innerWidth, height: innerHeight })
+        }
+        window.addEventListener('resize', onResize)
+        return () => {
+            window.removeEventListener('resize', this.onResize)
+        }
+    }, [])
+
+    return <div className="dimensions">{`${width}px - ${height}px`}</div>
+}
+
+export default Dimensions
