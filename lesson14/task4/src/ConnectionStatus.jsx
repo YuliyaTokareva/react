336fbd65196aae3e5343f4ea6184fff4a32ca09f
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-28 13:44:36 +0300

    add

--- lesson14/task4/src/ConnectionStatus.jsx
+++ lesson14/task4/src/ConnectionStatus.jsx
@@ -15,7 +15,7 @@ const ConnectionStatus = () => {
             window.removeEventListener('offline', setConnection)
             window.removeEventListener('online', setConnection)
         }
-    }, [window.navigator.onLine])
+    }, [window.navigator.onLine, statusOnline])
 
     return (
         <div className={`${statusOnline ? 'status_online' : 'status_offline'} status`}>
